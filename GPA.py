fa3cd6eb2d7572cd4aca7b8398a2f4b6c440768f
DamienSng <134802743+DamienSng@users.noreply.github.com>
2023-05-28 01:47:47 +0800

    GPA.py

--- GPA.py
+++ GPA.py
@@ -7,7 +7,7 @@ while True:
         creditP = float(input(f'enter the credits for module{i+1}: '))
         GPA=float(input(f'enter your GPA for module {i+1}: '))
         GP=GPA*creditP
-        TCredit+=+creditP
+        TCredit+=creditP
         TGP+=GP
 
     AGPA=(TGP/TCredit)
